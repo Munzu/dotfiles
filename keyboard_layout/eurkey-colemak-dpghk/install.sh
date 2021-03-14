e3e118cc7fee2b8e014da25b7819efaedcd1885b
Munzu <munzutai@gmail.com>
2021-03-14 20:00:50 +0100

    Update install script

--- keyboard_layout/eurkey-colemak-dpghk/install.sh
+++ keyboard_layout/eurkey-colemak-dpghk/install.sh
@@ -2,10 +2,10 @@
 # Technically a fork of https://gitlab.com/jungganz/eurkey-colemak-mod-dh
 
 # backup
-mv /usr/share/X11/xkb/symbols/eu ~/dotfiles/keyboard_layout/eurkey-colemak-dpghk/backup/eu
-mv /usr/share/X11/xkb/rules/evdev.extras.xml ~/dotfiles/keyboard_layout/eurkey-colemak-dpghk/backup/evdev.extras.xml
-mv /usr/share/X11/xkb/rules/base.extras.xml ~/dotfiles/keyboard_layout/eurkey-colemak-dpghk/backup/base.extras.xml
+mv /usr/share/X11/xkb/symbols/eu /home/tai/dotfiles/keyboard_layout/eurkey-colemak-dpghk/backup/eu
+mv /usr/share/X11/xkb/rules/evdev.extras.xml /home/tai/dotfiles/keyboard_layout/eurkey-colemak-dpghk/backup/evdev.extras.xml
+mv /usr/share/X11/xkb/rules/base.extras.xml /home/tai/dotfiles/keyboard_layout/eurkey-colemak-dpghk/backup/base.extras.xml
 
-sudo ln -s ~/dotfiles/keyboard_layout/eurkey-colemak-dpghk/eu /usr/share/X11/xkb/symbols/eu
-sudo ln -s ~/dotfiles/keyboard_layout/eurkey-colemak-dpghk/evdev.extras.xml /usr/share/X11/xkb/rules/evdev.extras.xml
-sudo ln -s ~/dotfiles/keyboard_layout/eurkey-colemak-dpghk/base.extras.xml /usr/share/X11/xkb/rules/base.extras.xml
+sudo ln -s /home/tai/dotfiles/keyboard_layout/eurkey-colemak-dpghk/eu /usr/share/X11/xkb/symbols/eu
+sudo ln -s /home/tai/dotfiles/keyboard_layout/eurkey-colemak-dpghk/evdev.extras.xml /usr/share/X11/xkb/rules/evdev.extras.xml
+sudo ln -s /home/tai/dotfiles/keyboard_layout/eurkey-colemak-dpghk/base.extras.xml /usr/share/X11/xkb/rules/base.extras.xml
